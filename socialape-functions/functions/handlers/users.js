21afb52500bb287a424a00c658ed71ff88867d7b
CipherGirl <ciphergirlmeow@gmail.com>
2020-09-11 23:53:49 +0600

    Image Upload (Unfinished)

--- socialape-functions/functions/handlers/users.js
+++ socialape-functions/functions/handlers/users.js
@@ -86,3 +86,14 @@ exports.login = (req, res) => {
       return res.status(500).json({ error: err.code });
     });
 };
+
+exports.uploadImage = (req, res) => {
+  const BusBoy = require("busboy");
+  const path = require("path");
+  const os = require("os");
+  const fs = require("fs");
+
+  const busboy = new BusBoy({ headers: req.headers });
+
+  busboy.on('file', (feildname, file, filename, ))
+};
--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -4,16 +4,15 @@ const app = require("express")();
 const FBAuth = require("./util/fbAuth");
 
 const { getAllScreams, postOneScream } = require("./handlers/screams");
-const { signup, login } = require("./handlers/users");
+const { signup, login, uploadImage } = require("./handlers/users");
 
-//*Users Routes
+//* Users Routes
 app.post("/signup", signup);
 app.post("/login", login);
+app.post("/user/image", uploadImage);
 
-//*Route for loading all the Screams
+//* Scream Routes
 app.get("/screams", getAllScreams);
-
-//*Route for posting a Scream
 app.post("/scream", FBAuth, postOneScream);
 
 exports.api = functions.https.onRequest(app);
